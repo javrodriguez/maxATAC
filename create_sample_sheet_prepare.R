41d9015987ffdf80466c0f0e35d126860772e24e
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-15 01:37:40 -0500

    Update create_sample_sheet_prepare.R

--- create_sample_sheet_prepare.R
+++ create_sample_sheet_prepare.R
@@ -1,11 +1,12 @@
 suffix=".dd.bam$"
-bam_dir="./BAM-DD"
+bam_dir="/BAM-DD/"
 
 wd=getwd()
+bam_dir=paste0(wd,bam_dir)
 fpath = list.files(bam_dir,pattern=suffix,full.names=T,include.dirs=F)
 fname = list.files(bam_dir,pattern=suffix,full.names=F,include.dirs=F)
 fname = gsub(x=fname,pattern=suffix,replacement="")
 
-m = cbind(fpath=paste0(wd,"/",fpath),outdir=paste0(fname,"-maxATAC_prepare"))
+m = cbind(fpath=fpath,outdir=paste0(fname,"-maxATAC_prepare"),fname=fname)
 print(m)
 write.table(m,"sample_sheet_prepare.txt",sep="\t",row.names=F,col.names=F,quote=F)
