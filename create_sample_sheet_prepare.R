1a05b377e8b2d6dae18a44c2f779e623b69a76ff
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-14 23:22:20 -0500

    Create create_sample_sheet_prepare.R

---
+++ create_sample_sheet_prepare.R
@@ -0,0 +1,10 @@
+suffix=".dd.bam"
+bam_dir="./BAM-DD"
+
+fpath = list.files(bam_dir,pattern=suffix,full.names=T,include.dirs=F)
+fname = list.files(bam_dir,pattern=suffix,full.names=F,include.dirs=F)
+fname = gsub(x=fname,pattern=suffix,replacement="")
+
+m = cbind(fpath,outdir=paste0(fname,"-maxATAC_prepare"))
+print(m)
+write.table(m,"sample_sheet_prepare.txt",sep="\t",row.names=F,col.names=F,quote=F)
