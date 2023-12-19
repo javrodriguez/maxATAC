ff10f6fe5141711bd0034cfae179cdd6e48f8d5e
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2023-12-18 21:06:07 -0500

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --job-name=maxatac-prepare
 #SBATCH --output=logs-prepare/%x-%j.out
-#SBATCH --error=logs-[re[are/%x-%j.out
+#SBATCH --error=logs-prepare/%x-%j.err
 #SBATCH --mem-per-cpu=3G
 #SBATCH --cpus-per-task=8
 #SBATCH --time=24:00:00
@@ -16,11 +16,14 @@ fname=$(awk "NR==${SLURM_ARRAY_TASK_ID} {print \$3}" sample_sheet_prepare.txt)
 echo $bam
 echo $outname
 
-#module load condaenvs/gpu/maxatac
-module load anaconda3/gpu/5.2.0
-conda activate maxatac
-module unload anaconda3/gpu/5.2.0
-export PATH=/gpfs/share/apps/ucscutils/374:$PATH
+module purge
+module load condaenvs/gpu/maxatac
+module add openssl/1.0.2
+
+#module load anaconda3/gpu/5.2.0
+#conda activate maxatac
+#module unload anaconda3/gpu/5.2.0
+#export PATH=/gpfs/share/apps/ucscutils/374:$PATH
 
 maxatac prepare \
     --chrom_sizes /gpfs/home/rodrij92/opt/maxatac/data/hg38/hg38.chrom.sizes \
