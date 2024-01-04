5f3ec75988d06ef3715db1df13fd0d8333e6a2e0
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2024-01-04 13:57:28 -0500

    Update create_sample_sheet_celltype_tcga.R

--- create_sample_sheet_celltype_tcga.R
+++ create_sample_sheet_celltype_tcga.R
@@ -7,9 +7,5 @@ setwd(maxATAC_dir)
 fpath = list.files(maxATAC_dir,pattern=suffix,full.names=T,include.dirs=F,recursive = T)
 fname = list.files(maxATAC_dir,pattern=suffix,full.names=F,include.dirs=F,recursive = T)
 fname = gsub(x=fname,pattern=paste0("/",suffix),replacement="")
-
 m = as.data.frame(cbind(sample_name=fname,celltype=NA))
-x=cSplit(indt = m,splitCols = "sample_name",sep = "_")
-m$celltype=x$sample_name_01
-
 write.csv(m,"sample_sheet_celltype.csv",row.names=F)
