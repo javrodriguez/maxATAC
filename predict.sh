6504abc960f40a4285fe08b95fcfe7c35d692ead
javrodriguez <javier.rodriguez.hernaez@gmail.com>
2024-01-24 15:38:21 -0500

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#SBATCH -J maxatac_predict
-#SBATCH --mem=50gb
-#SBATCH --time=48:00:00
+#SBATCH-J maxatac_predict
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8 
+#SBATCH --mem-per-cpu=8G
 #SBATCH --output=logs-prediction/%J.out
 #SBATCH --error=logs-prediction/%J.err
 
@@ -17,13 +20,10 @@ module purge
 module load condaenvs/gpu/maxatac
 module add openssl/1.0.2
 
-#module load anaconda3/gpu/5.2.0
-#conda activate maxatac
-#module unload anaconda3/gpu/5.2.0
-
 maxatac predict -tf CTCF  \
   --signal $bigwig \
   -o $outname \
   --sequence /gpfs/home/rodrij92/opt/maxatac/data/hg38/hg38.2bit \
   --blacklist /gpfs/home/rodrij92/opt/maxatac/data/hg38/hg38_maxatac_blacklist.bed \
-  --chromosome_sizes /gpfs/home/rodrij92/opt/maxatac/data/hg38/hg38.chrom.sizes
+  --chromosome_sizes /gpfs/home/rodrij92/opt/maxatac/data/hg38/hg38.chrom.sizes \
+  #--chromosomes chr1
